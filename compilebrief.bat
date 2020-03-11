a5ef7b70c3b4264d3ca4e7903eb12f271656df23
Phineas Greene <phinsfishes@gmail.com>
2020-03-11 16:57:21 -0500

    sum stuff

--- compilebrief.bat
+++ compilebrief.bat
@@ -1,7 +1,3 @@
 sed -r -f "C:\Users\phine\Documents\Debate\Brief Markup\sedbrief.txt" "%1.txt" > "%1.tex"
 pdflatex "%1.tex"
-del brief.aux
-del brief.out
-del brief.tex
-del brief.toc
-del brief.log
+del "%1.log"
