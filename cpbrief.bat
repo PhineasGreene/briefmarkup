2863028a0c59c9992d55987d1150963d5be76cdf
Phineas Greene <phinsfishes@gmail.com>
2020-03-12 11:39:06 -0500

    Added more error handling.\nMoved TOC, TITLE command outside of sed script.

--- cpbrief.bat
+++ cpbrief.bat
@@ -1,8 +1,14 @@
-IF %1.==. GOTO END1
+@echo off
+IF %1.==. GOTO E1
 sed -r -f "C:\Users\phine\Documents\Debate\Brief Markup\sedbrief.txt" "%1.txt" > "%1.tex"
+del "%1.pdf"
+IF %ERRORLEVEL% neq 0 GOTO E2
 pdflatex "%1.tex"
 del "%1.log"
-GOTO END2
-:END1
+GOTO END
+:E1
 echo ERROR: No filename. Please provide the name of the text file you would like to compile.
-:END2
+GOTO END
+:E2
+echo ERROR: One or more of the files cpbrief needs to modify are open somewhere (check the browser). Close them and try again.
+:END
